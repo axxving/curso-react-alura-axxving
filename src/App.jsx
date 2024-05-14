3f9e62a9d9a9514d61782addd2228b6eb3c4273c
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-05-13 18:02:56 -0600

    feat(): add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Header } from './components/Header'
 import { Hero } from './components/Hero'
+import { Formulario } from './components/Formulario'
 //import reactLogo from './assets/react.svg';
 //import viteLogo from '/vite.svg';
 
@@ -9,6 +10,7 @@ export const App = () => {
     <>
       <Header />
       <Hero />
+      <Formulario />
     </>
 
   )
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { EnvelopeFill, PersonFill, TelephoneFill, ChatDotsFill } from 'react-bootstrap-icons';
+
+export const Formulario = () => {
+    return (
+        <Container fluid className='formulario'>
+            <Container>
+                <h2>Contáctanos</h2>
+                <Form>
+                    <Row>
+                        <Col md={6}>
+                            <Form.Group controlId="formName">
+                                <Form.Control className='form-input' type="text" placeholder="Nombre" />
+                            </Form.Group>
+                        </Col>
+                        <Col md={6}>
+                            <Form.Group controlId="formEmail">
+                                <Form.Control className='form-input' type="email" placeholder="Correo Electrónico" />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={6}>
+                            <Form.Group controlId="formPhone">
+                                <Form.Control className='form-input' type="tel" placeholder="Teléfono" />
+                            </Form.Group>
+                        </Col>
+                        <Col md={6}>
+                            <Form.Group controlId="formSubject">
+                                <Form.Control className='form-input' type="text" placeholder="Asunto" />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Form.Group controlId="formMessage">
+                        <Form.Control className='form-input' as="textarea" rows={4} placeholder="Mensaje" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit"><ChatDotsFill className="mr-2 mt-2" /> Enviar Mensaje</Button>
+                </Form>
+            </Container>
+        </Container>
+    );
+};
