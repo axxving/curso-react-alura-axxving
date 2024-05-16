752f80265026dc96ed9e13efbfea69045bae60eb
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-05-16 17:54:31 -0600

    feat(): object with input values

--- src/components/Campo.jsx
+++ src/components/Campo.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { Col, Form } from 'react-bootstrap';
 
-export const Campo = ({ controlId, type, placeholder, titulo, required }) => {
+export const Campo = ({ controlId, type, placeholder, valorr, titulo, required, setValor }) => {
+
+    const manejarCambio = (e) => {
+        setValor(e.target.value);
+    }
+
     return (
         <Col md={6}>
             <Form.Group controlId={controlId}>
-                <Form.Control titulo={titulo} className='form-input' type={type} placeholder={placeholder} required={required} />
+                <Form.Control 
+                    titulo={titulo} 
+                    className='form-input' 
+                    type={type} 
+                    placeholder={placeholder} 
+                    required={required}
+                    value={valorr}
+                    onChange={manejarCambio}
+                />
             </Form.Group>
         </Col>
     );
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { Container, Row, Form } from 'react-bootstrap';
 import { Campo } from './Campo';
 import { ListaOpciones } from './listaOpciones/ListaOpciones';
@@ -6,10 +6,23 @@ import { Boton } from './listaOpciones/Boton';
 
 export const Formulario = () => {
 
+    const [nombre, setNombre] = useState("");
+    const [apellido, setApellido] = useState("");
+    const [puesto, setPuesto] = useState("");
+    const [foto, setFoto] = useState("");
+
     const manejarEnvio = (e) => {
-        console.log('Manejar el envio', e);
+        console.log('Manejar el envio');
         // Evitando el envio del formulario
         e.preventDefault();
+        let datosAenviar = {
+            nombre,
+            apellido,
+            puesto,
+            foto,
+        }
+
+        console.log(datosAenviar);
     }
 
     return (
@@ -24,6 +37,8 @@ export const Formulario = () => {
                             type="text"
                             placeholder="Nombre"
                             required
+                            valor={nombre}
+                            setValor={setNombre}
                         />
                         <Campo
                             titulo="Apellido"
@@ -31,22 +46,28 @@ export const Formulario = () => {
                             type="text"
                             placeholder="Apellido"
                             required
+                            valor={apellido}
+                            setValor={setApellido}
                         />
                     </Row>
                     <Row>
-                        <Campo
-                            titulo="Foto"
-                            controlId="formSubject"
-                            type="text"
-                            placeholder="Foto"
-                            required
-                        />
                         <Campo
                             titulo="Puesto"
                             controlId="formPhone"
                             type="tel"
                             placeholder="Puesto"
                             required
+                            valor={puesto}
+                            setValor={setPuesto}
+                        />
+                        <Campo
+                            titulo="Foto"
+                            controlId="formSubject"
+                            type="text"
+                            placeholder="Foto"
+                            required
+                            valor={foto}
+                            setValor={setFoto}
                         />
                     </Row>
                     <ListaOpciones />
